5c7d042b6ac82fc7333ca8659d70d39d488eadee
Guilherme Lacerda <lacerdagui42@gmail.com>
2022-09-14 20:18:31 -0300

    Config TypeScript Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ app.get('/ads', (request, response) => {
     return response.json([
         {id: 1, name: 'Anúncio 1'},
         {id: 2, name: 'Anúncio 2'},
-        {id: 3, name: 'Anúncio 3'}
+        {id: 3, name: 'Anúncio 3'},
     ]); 
 });
 
